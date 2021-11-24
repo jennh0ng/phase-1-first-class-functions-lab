21ec9b4456a16727a5e0769b229bfb82c014615e
Jennifer Hong <hongjenn24@gmail.com>
2021-11-23 19:24:33 -0800

    commit tree cd29a073d717c6a6ff884b6c8ae814c214cdbe14

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    const returnFirstTwo = function() {
+        return drivers.slice(0,2);
+    }
+    return returnFirstTwo();
+}
+
+function returnLastTwoDrivers(drivers) {
+    const returnLastTwo = function() {
+        return drivers.slice((drivers.length-2), drivers.length);
+    }
+    return returnLastTwo();
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    const fareMult = function(price) {
+        return (fare * price);
+    }
+    return fareMult;
+}
+
+function fareDoubler(fare) {
+    const double = createFareMultiplier(fare);
+        return double(2);
+    }
+
+function fareTripler(fare) {
+    const triple = createFareMultiplier(fare);
+        return triple(3);
+    }
+
+function selectDifferentDrivers(drivers, someFunction) {
+    return someFunction(drivers);
+}
